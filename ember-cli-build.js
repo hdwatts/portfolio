0235ea21186485de4cc91eaa902835c343880424
hdwatts <howard@hdwatts.com>
2016-07-06 16:19:55 -0400

    Added favicon and fixed portrait shadow

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -8,6 +8,10 @@ module.exports = function(defaults) {
     // Add options here
     'ember-bootstrap': {
       'importBootstrapTheme': true
+    },
+    favicon: {
+      config: {
+      }
     }
   });
 
